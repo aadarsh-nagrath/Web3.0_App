cf24830b08e4de5a8bffa038598f7b8c43888e1a
Aadarsh Nagrath <anagrath1@gmail.com>
2023-10-22 02:20:01 +0530

    shifted to polygon

--- CF/client/src/main.jsx
+++ CF/client/src/main.jsx
@@ -10,7 +10,7 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <ThirdwebProvider activeChain="goerli"> 
+  <ThirdwebProvider activeChain={{chainId:80001,rpc: ["https://rpc.ankr.com/polygon_mumbai"]}}> 
     <Router>
       <StateContextProvider>
         <App />
--- CF/web3/hardhat.config.js
+++ CF/web3/hardhat.config.js
@@ -2,11 +2,11 @@
 module.exports = {
   solidity: {
     version: '0.8.9',
-    defaultNetwork: 'goerli',
+    defaultNetwork: 'polygon',
     networks: {
       hardhat: {},
-      goerli: {
-        url: 'https://rpc.ankr.com/eth_goerli',
+      polygon: {
+        url: 'https://rpc.ankr.com/polygon_mumbai',
         accounts: [`0x${process.env.PRIVATE_KEY}`]
       }
     },
